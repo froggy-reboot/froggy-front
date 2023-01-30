fbae0029166079da8cff4a5e43b73d639970e9f0
s0ojin <onepst@hanyang.ac.kr>
2023-01-30 18:06:06 +0900

    initial setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex h-[100vh] items-center justify-center text-5xl font-bold text-green-500">
+        hello froggy
+      </div>
+      <ReactQueryDevtools />
+    </>
   );
 }
 
