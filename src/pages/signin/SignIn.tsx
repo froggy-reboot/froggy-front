472f509691ec66b58618ebf3bf380cc5b274cf82
s0ojin <onepst@hanyang.ac.kr>
2023-02-20 22:46:40 +0900

    Refactor : 상수분리

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { ReactComponent as Logo } from 'src/assets/logo.svg';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import {
+  SOCIAL_LOGIN,
+  ERROR_MESSAGE,
+  LOGIN,
+  SIGNUP,
+  REG_EXP,
+} from 'src/pages/signin/SignInConstants';
 
 interface IFormInput {
   email: string;
@@ -14,6 +21,7 @@ export default function SignIn() {
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<IFormInput>({ mode: 'all' });
+
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     console.log(data);
   };
@@ -29,9 +37,8 @@ export default function SignIn() {
             {...register('email', {
               required: true,
               pattern: {
-                value:
-                  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
-                message: '이메일 형식이 올바르지 않습니다.',
+                value: REG_EXP.EMAIL,
+                message: ERROR_MESSAGE.EMAIL,
               },
             })}
             className="h-[4.8rem] w-[100%] rounded-[0.8rem] bg-black-10 px-[1.6rem] text-Body focus:outline-green-50"
@@ -42,8 +49,8 @@ export default function SignIn() {
             {...register('password', {
               required: true,
               pattern: {
-                value: /.{6,}/,
-                message: '비밀번호는 6글자 이상 입력해주세요.',
+                value: REG_EXP.PASSWORD,
+                message: ERROR_MESSAGE.PASSWORD,
               },
             })}
             className="h-[4.8rem] w-[100%] rounded-[0.8rem] bg-black-10 px-[1.6rem] text-Body focus:outline-green-50"
@@ -57,21 +64,26 @@ export default function SignIn() {
             className={`mt-[1rem] h-[5rem] w-[100%] rounded-[1rem] text-Body text-white md:mt-[2rem] ${
               isValid ? 'bg-green-50' : 'bg-black-30'
             }`}>
-            로그인
+            {LOGIN}
           </button>
           <hr className=" mt-[3.5rem] w-[100%] overflow-visible border-black-50 text-center text-Callout font-normal text-black-50 after:relative after:bottom-4 after:bg-white after:px-5 after:content-['sns_로그인'] md:mt-[5rem] md:after:text-Tag" />
         </div>
       </form>
       <ul className="mt-[3rem] flex gap-[2rem] md:mt-[4rem] md:gap-[6rem]">
-        <li className="h-[5rem] w-[5rem] rounded-full bg-black-10 md:h-[7rem] md:w-[7rem]"></li>
-        <li className="h-[5rem] w-[5rem] rounded-full bg-black-10 md:h-[7rem] md:w-[7rem]"></li>
-        <li className="h-[5rem] w-[5rem] rounded-full bg-black-10 md:h-[7rem] md:w-[7rem]"></li>
-        <li className="h-[5rem] w-[5rem] rounded-full bg-black-10 md:h-[7rem] md:w-[7rem]"></li>
+        {SOCIAL_LOGIN.map((social) => {
+          return (
+            <li
+              key={social.id}
+              className="h-[5rem] w-[5rem] rounded-full bg-black-10 md:h-[7rem] md:w-[7rem]">
+              <img src="" alt={social.type} />
+            </li>
+          );
+        })}
       </ul>
       <p className="absolute bottom-[4rem] my-0 mx-auto text-Callout font-normal text-black-50 md:bottom-[6rem] md:text-Tag">
-        froggy는 처음이신가요?
+        {SIGNUP.MESSAGE}
         <Link to={'/'} className="ml-2 text-green-100 md:ml-3">
-          회원가입
+          {SIGNUP.SIGNUP}
         </Link>
       </p>
     </div>
---
+++ src/pages/signin/SignInConstants.ts
@@ -0,0 +1,38 @@
+const SOCIAL_LOGIN = [
+  {
+    id: 1,
+    type: 'raverlry',
+  },
+  {
+    id: 2,
+    type: 'google',
+  },
+  {
+    id: 3,
+    type: 'kakao',
+  },
+  {
+    id: 4,
+    type: 'naver',
+  },
+] as const;
+
+const ERROR_MESSAGE = {
+  EMAIL: '이메일 형식이 올바르지 않습니다.',
+  PASSWORD: '비밀번호는 6글자 이상 입력해주세요.',
+} as const;
+
+const LOGIN = '로그인' as const;
+
+const SIGNUP = {
+  MESSAGE: 'froggy는 처음이신가요?',
+  SIGNUP: '회원가입',
+} as const;
+
+const REG_EXP = {
+  EMAIL:
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
+  PASSWORD: /.{6,}/,
+} as const;
+
+export { SOCIAL_LOGIN, ERROR_MESSAGE, LOGIN, SIGNUP, REG_EXP };
