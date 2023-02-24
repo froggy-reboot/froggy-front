86d8df5347c284fbcba62834e8415f9fec82430b
s0ojin <onepst@hanyang.ac.kr>
2023-02-24 23:44:35 +0900

    Fix : 소셜로그인 로직 변경

--- src/apis/signInApi.ts
+++ src/apis/signInApi.ts
@@ -1,17 +1,35 @@
 import { IFormInput } from 'src/pages/signin/SignIn';
 import { publicApi } from 'src/apis/authApi';
+import axios from 'axios';
 
 export async function getGoogleRegister() {
-  const response = await publicApi.get('api/v1/auth/google/register');
-  return response;
+  try {
+    await publicApi.get('api/v1/auth/google/register');
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      window.open(error.response?.data);
+    }
+  }
 }
+
 export async function getNaverRegister() {
-  const response = await publicApi.get('/api/v1/auth/naver/register');
-  return response;
+  try {
+    await publicApi.get('/api/v1/auth/naver/register');
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      window.open(error.response?.data);
+    }
+  }
 }
+
 export async function getRavelryRegister() {
-  const response = await publicApi.get('/api/v1/auth/ravelry/register');
-  return response;
+  try {
+    await publicApi.get('/api/v1/auth/ravelry/register');
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      window.open(error.response?.data);
+    }
+  }
 }
 
 export async function postEmailLogin(data: IFormInput) {
