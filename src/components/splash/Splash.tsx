a4d59f25213ee723ab914c6ba2681289b32c47cc
s0ojin <onepst@hanyang.ac.kr>
2023-02-27 17:05:50 +0900

    Design : 스플래시 반응형 디자인

--- src/components/splash/Splash.tsx
+++ src/components/splash/Splash.tsx
@@ -6,9 +6,17 @@ import { ReactComponent as Logo } from 'src/assets/logo_yellow.svg';
 export default function Splash() {
   return (
     <div className="container bg-green-50">
-      <img src={spinner} alt="loading..." className="z-[1] mt-[14rem]" />
-      <img src={ellipse} alt="loading..." className="absolute top-[35.6rem]" />
-      <Logo className="absolute bottom-[35.6rem] w-[18.8rem]" />
+      <img
+        src={spinner}
+        alt="loading..."
+        className="z-[1] mt-[14rem] md:mt-[19.4rem] md:w-[43rem]"
+      />
+      <img
+        src={ellipse}
+        alt="loading..."
+        className="absolute top-[35.6rem] md:top-[43rem] md:w-[22rem]"
+      />
+      <Logo className="absolute bottom-[35.6rem] w-[18.8rem] md:bottom-[43rem] md:w-[26rem]" />
     </div>
   );
 }
