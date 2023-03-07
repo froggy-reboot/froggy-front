c129947838af829b0aaa07b0d27403aafa080516
s0ojin <onepst@hanyang.ac.kr>
2023-03-07 16:41:56 +0900

    Feat : 인증 페이지 분리 로직 추가

--- src/routes/RoutePage.tsx
+++ src/routes/RoutePage.tsx
@@ -3,13 +3,24 @@ import { Route, Routes } from 'react-router-dom';
 import SignIn from 'src/pages/signin/SignIn';
 import SignUp from 'src/pages/signup/SignUp';
 import OauthRedirectHandler from 'src/pages/signin/OauthRedirectHandler';
+import PrivateRoute from 'src/routes/PrivateRoute';
 
 export default function RoutePage() {
   return (
     <Routes>
-      <Route path="/:userId" element={<OauthRedirectHandler />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
+      {/* 인증과 관계없는 페이지 */}
+      <Route
+        path="/sign-in/social/:userId"
+        element={<OauthRedirectHandler />}
+      />
+      <Route element={<PrivateRoute authentication={true} />}>
+        {/* 비로그인이면 볼 수 없는 페이지 */}
+      </Route>
+      <Route element={<PrivateRoute authentication={false} />}>
+        {/* 로그인이면 볼 수 없는 페이지 */}
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Route>
     </Routes>
   );
 }
