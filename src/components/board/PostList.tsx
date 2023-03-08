144fb702fd1ff3e39438fab4e79220e4975bc71a
s0ojin <onepst@hanyang.ac.kr>
2023-03-08 16:32:06 +0900

    Design : 게시판 메인 페이지 폰트 사이즈 재조정

--- src/components/board/PostList.tsx
+++ src/components/board/PostList.tsx
@@ -78,36 +78,34 @@ const Dummy = [
 
 export default function PostList() {
   return (
-    <ul>
+    <ul className="flex flex-col gap-[0.7rem]">
       {Dummy.map((post) => (
-        <>
+        <li key={post.post_id} className="pl-[0.4rem]">
           <hr className="border-black-30" />
-          <li key={post.post_id} className="mb-[0.7rem] h-[9rem] pl-[0.4rem]">
-            <p className="mt-[0.6rem] text-BoardSub font-medium text-black-50">
-              {post.author}
+          <p className="mt-[0.7rem] text-[12px] font-medium text-black-50">
+            {post.author}
+          </p>
+          <div className="mt-[0.5rem] flex">
+            <span className="mini_btn inline-block h-[1.9rem] w-[3.8rem] text-center text-[12px] leading-[1.9rem]">
+              {post.type}
+            </span>
+            <p className="ml-[0.9rem] inline-block text-Body font-medium">
+              {post.title}
             </p>
-            <div className="mt-[0.5rem]">
-              <span className="mini_btn inline-block h-[1.9rem] w-[3.8rem] text-center text-Board leading-[1.9rem]">
-                {post.type}
-              </span>
-              <p className="ml-[0.9rem] inline-block text-Tag">{post.title}</p>
+          </div>
+          <p className="mt-[0.5rem] text-Caption font-normal text-black-50">
+            {post.preview}
+          </p>
+          <div className="mt-[0.7rem] mb-[0.4rem] flex items-center justify-between text-[12px]">
+            <div>
+              <LikeIcon className="mr-[0.3rem] inline-block h-[1.5rem] w-[1.5rem] fill-black-50" />
+              <span>{post.likeCount}</span>
+              <ChatIcon className="mr-[0.3rem] ml-[0.4rem] inline-block h-[1.5rem] w-[1.5rem] fill-black-50" />
+              <span>{post.commentCount}</span>
             </div>
-            <p className="mt-[0.3rem] text-Board text-black-50">
-              {post.preview}
-            </p>
-            <div className="mt-[0.8rem] flex justify-between">
-              <div>
-                <LikeIcon className="mr-[0.3rem] inline-block h-[1.5rem] w-[1.5rem] fill-black-50" />
-                <span>{post.likeCount}</span>
-                <ChatIcon className="mr-[0.3rem] ml-[0.4rem] inline-block h-[1.5rem] w-[1.5rem] fill-black-50" />
-                <span>{post.commentCount}</span>
-              </div>
-              <p className="pr-[0.8rem] text-Board text-black-50">
-                {post.date}
-              </p>
-            </div>
-          </li>
-        </>
+            <p className="pr-[0.8rem] text-Board text-black-50">{post.date}</p>
+          </div>
+        </li>
       ))}
     </ul>
   );
