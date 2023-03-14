b098b30db66f586b6d32bc978f4ad7c3a1b6bad2
s0ojin <onepst@hanyang.ac.kr>
2023-03-14 15:33:43 +0900

    Refactor : boardMain - 글생성 모달 분리

--- src/components/modals/Modals.tsx
+++ src/components/modals/Modals.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { useModal } from 'src/hooks/useModal';
 import { ComponentProps, FunctionComponent } from 'react';
 import UpdateDeleteModal from 'src/components/modals/UpdateDeleteModal';
+import CreatePostModal from 'src/components/modals/CreatePostModal';
 
 export const modals = {
   UpdateDeleteModal: UpdateDeleteModal as FunctionComponent<
     ComponentProps<typeof UpdateDeleteModal>
+  >,
+  CreatePostModal: CreatePostModal as FunctionComponent<
+    ComponentProps<typeof CreatePostModal>
   >,
 };
 
