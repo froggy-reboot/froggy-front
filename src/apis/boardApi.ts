3cca5ab7964d6daad93825601e7a2f594f8687b9
s0ojin <onepst@hanyang.ac.kr>
2023-03-20 16:50:32 +0900

    Fix : 게시글과 댓글 api분리

--- src/apis/boardApi.ts
+++ src/apis/boardApi.ts
@@ -9,3 +9,10 @@ export async function getArticleDetail(postId: string) {
   const response = await publicApi.get(`/api/v1/articles/${postId}`);
   return response;
 }
+
+export async function getCommets(articleId: number, { pageParam = 1 }) {
+  const response = await publicApi.get(
+    `/api/v1/articles/${articleId}/comments/pages/${pageParam}`,
+  );
+  return response;
+}
--- src/components/board/Comments.tsx
+++ src/components/board/Comments.tsx
@@ -3,31 +3,39 @@ import { ReactComponent as MenuIcon } from 'src/assets/menu.svg';
 import { useModal } from 'src/hooks/useModal';
 import { modals } from '../modals/Modals';
 import timeConverter from 'src/utils/timeConverter/timeConverter';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getCommets } from 'src/apis/boardApi';
 
-export interface ICommentData {
+/* export interface ICommentData {
   id: number;
   article_id: number;
   writer_id: number;
   content: string;
   created_at: string;
   deleted_at: string;
-}
+} */
 
-export default function Comment({ comments }: { comments: ICommentData[] }) {
+export default function Comment({ articleId }) {
   const { openModal } = useModal();
+  /*   const { isLoading, data } = useInfiniteQuery(
+    ['comments'],
+    ({ pageParam = 1 }) => getCommets(articleId, { pageParam }),
+  ); */
+
   return (
-    <ul className="flex h-[100%] flex-col gap-[0.2rem]">
+    <div>댓글 준비중입니다.</div>
+    /*     <ul className="flex h-[100%] flex-col gap-[0.2rem]">
       {comments.map((comment, idx) => (
         <>
           <li key={comment.id} className="flex">
             <img
-              src={/* comment.profile */ ''}
+              src={''}
               className="mt-[0.5rem] h-[3.2rem] w-[3.2rem] rounded-full"
             />
             <div className="ml-[0.9rem] mt-[0.9rem] flex flex-1 flex-col">
               <div className="flex h-[1.6rem] items-center">
                 <p className="mr-auto text-Tag font-bold">
-                  {/* comment.nickname */ '닉네임'}
+                  {'닉네임'}
                 </p>
                 <p className="text-Board text-black-50">
                   {timeConverter(comment.created_at)}
@@ -51,6 +59,6 @@ export default function Comment({ comments }: { comments: ICommentData[] }) {
           )}
         </>
       ))}
-    </ul>
+    </ul> */
   );
 }
