4b03a27b3b75f178470004c845ede36247efe47f
s0ojin <onepst@hanyang.ac.kr>
2023-03-21 17:53:19 +0900

    Fix : 게시글 이동 오류 수정

--- src/components/board/PostList.tsx
+++ src/components/board/PostList.tsx
@@ -44,31 +44,34 @@ export default function PostList() {
       {postList.length &&
         postList.map((page) => (
           <li key={page.id} className="mb-[0.7rem] h-[9rem] pl-[0.4rem]">
-            <Link to={`/board/${page.id}`} />
-            <hr className="border-black-30" />
-            <p className="mt-[0.6rem] text-BoardSub font-medium text-black-50">
-              {page.user?.nickname}
-            </p>
-            <div className="mt-[0.5rem]">
-              <span className="mini_btn inline-block h-[1.9rem] w-[3.8rem] text-center text-Board leading-[1.9rem]">
-                {page.articleType}
-              </span>
-              <p className="ml-[0.9rem] inline-block text-Tag">{page.title}</p>
-            </div>
-            <p className="mt-[0.3rem] text-Board text-black-50">
-              {page.content}
-            </p>
-            <div className="mt-[0.8rem] flex justify-between">
-              <div>
-                <LikeIcon className="mr-[0.3rem] inline-block h-[1.5rem] w-[1.5rem] fill-black-50" />
-                <span>{page.liked}</span>
-                <ChatIcon className="mr-[0.3rem] ml-[0.4rem] inline-block h-[1.5rem] w-[1.5rem] fill-black-50" />
-                <span>{page.commentCount}</span>
-              </div>
-              <p className="pr-[0.8rem] text-Board text-black-50">
-                {timeConverter(page.createdAt)}
+            <Link to={`/board/${page.id}`}>
+              <hr className="border-black-30" />
+              <p className="mt-[0.6rem] text-BoardSub font-medium text-black-50">
+                {page.user?.nickname}
               </p>
-            </div>
+              <div className="mt-[0.5rem]">
+                <span className="mini_btn inline-block h-[1.9rem] w-[3.8rem] text-center text-Board leading-[1.9rem]">
+                  {page.articleType}
+                </span>
+                <p className="ml-[0.9rem] inline-block text-Tag">
+                  {page.title}
+                </p>
+              </div>
+              <p className="mt-[0.3rem] text-Board text-black-50">
+                {page.content}
+              </p>
+              <div className="mt-[0.8rem] flex justify-between">
+                <div>
+                  <LikeIcon className="mr-[0.3rem] inline-block h-[1.5rem] w-[1.5rem] fill-black-50" />
+                  <span>{page.liked}</span>
+                  <ChatIcon className="mr-[0.3rem] ml-[0.4rem] inline-block h-[1.5rem] w-[1.5rem] fill-black-50" />
+                  <span>{page.commentCount}</span>
+                </div>
+                <p className="pr-[0.8rem] text-Board text-black-50">
+                  {timeConverter(page.createdAt)}
+                </p>
+              </div>
+            </Link>
           </li>
         ))}
     </ul>
