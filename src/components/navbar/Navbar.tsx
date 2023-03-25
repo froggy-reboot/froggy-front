6553ba0199c8f9c88f20e8703dd6d882890646b5
s0ojin <onepst@hanyang.ac.kr>
2023-03-26 00:14:37 +0900

    Feat : 댓글 네브바 적용

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { icons } from 'src/assets/navbar';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
+import CommandNavBar from './CommandNavbar';
 
 function Navbar() {
+  const location = useLocation();
   const navbarIcon = [
     { id: 'Home', link: '/', fill: icons.homeActive, empty: icons.home },
     { id: 'Knit', link: '/feed', fill: icons.knitActive, empty: icons.knit },
@@ -41,7 +43,7 @@ function Navbar() {
 
   return (
     <div className="fixed bottom-0 flex h-[8.3rem] w-screen items-center justify-evenly rounded-[15px_15px_0px_0px] bg-white pb-[2rem] align-middle shadow-[0px_-1px_3px_rgba(0,0,0,0.15)]">
-      {navbarBtns}
+      {location.pathname === '/board/:postId' ? <CommandNavBar /> : navbarBtns}
     </div>
   );
 }
