e49858ae8640e744c20595b9a9c0348fac26588d
s0ojin <onepst@hanyang.ac.kr>
2023-03-27 18:48:49 +0900

    Fix : 리프레시토큰 트리거 안되는 오류 수정

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { LOGIN } from 'src/pages/signin/SignInConstants';
 
 export const publicApi = axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_IP}`,
@@ -11,9 +12,52 @@ export const privateApi = axios.create({
   },
 });
 
-export async function postRefreshToken() {
-  const response = await publicApi.post('/api/v1/auth/refresh', {
+function postRefreshToken() {
+  const response = publicApi.post('/api/v1/auth/refresh', {
     refreshToken: localStorage.getItem('refreshToken'),
   });
   return response;
 }
+
+//리프레시 토큰
+privateApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+
+    if (status === 401) {
+      if (error.response.data.message === 'Unauthorized') {
+        const originRequest = config;
+        try {
+          const tokenResponse = await postRefreshToken();
+          if (tokenResponse.status === 201) {
+            const newAccessToken = tokenResponse.data.token;
+            localStorage.setItem('accessToken', tokenResponse.data.token);
+            localStorage.setItem(
+              'refreshToken',
+              tokenResponse.data.refreshToken,
+            );
+            axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+            originRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+            return axios(originRequest);
+          }
+        } catch (error) {
+          if (axios.isAxiosError(error)) {
+            if (error.response?.status === 404) {
+              alert(LOGIN.MESSAGE.EXPIRED);
+              window.location.replace('/sign-in');
+            } else {
+              alert(LOGIN.MESSAGE.ETC);
+            }
+          }
+        }
+      }
+    }
+    return Promise.reject(error);
+  },
+);
