782572a50786b81d855bc96c353f068075b2c35c
s0ojin <onepst@hanyang.ac.kr>
2023-03-28 15:24:57 +0900

    Feat : 게시글 및 댓글 삭제 연결 후속처리

--- src/components/modals/ConfirmModal.tsx
+++ src/components/modals/ConfirmModal.tsx
@@ -2,26 +2,38 @@ import React from 'react';
 import { deleteArticle, deleteComment } from 'src/apis/boardApi';
 import { useModal } from 'src/hooks/useModal';
 import { modals } from 'src/components/modals/Modals';
+import { useNavigate } from 'react-router-dom';
 
 export default function ConfirmModal() {
   const { closeModal, showModal } = useModal();
-  const deleteConfirmHandler = () => {
+  const navigate = useNavigate();
+
+  const deleteConfirmHandler = async () => {
+    closeModal(modals.ConfirmModal);
     if (showModal[0].props.commentId) {
       try {
-        deleteComment(showModal[0].props.postId, showModal[0].props.commentId);
+        const response = await deleteComment(
+          showModal[0].props.postId,
+          showModal[0].props.commentId,
+        );
+        if (response.status === 200) {
+          alert('댓글을 삭제하였습니다.');
+        }
       } catch (error) {
         console.log(error);
       }
     }
     if (!showModal[0].props.commentId) {
       try {
-        deleteArticle(showModal[0].props.postId);
+        const response = await deleteArticle(showModal[0].props.postId);
+        if (response.status === 200) {
+          alert('게시글을 삭제하였습니다.');
+          navigate('/board');
+        }
       } catch (error) {
         console.log(error);
       }
     }
-
-    closeModal(modals.ConfirmModal);
   };
 
   const cancelHandler = () => {
