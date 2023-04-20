733f0a14c6f83795cf5b52e5cab981154f791803
정하림 <harimchung1998@gmail.com>
2023-04-20 17:57:12 +0900

    profile update modal 수정

--- src/components/modals/ProfileUpdateModal.tsx
+++ src/components/modals/ProfileUpdateModal.tsx
@@ -7,15 +7,22 @@ import { modals } from 'src/components/modals/Modals';
 
 function ProfileUpdateModal() {
   const { openModal, closeModal, showModal } = useModal();
+  const updateHandler = () => {
+    console.log("hello")
+  }
+  const deleteHandler = () => {
+    // fileInput.current.click()
+    console.log("hello")
+  }
   return (
     <div className="modal_bg items-end"
-    onClick={() => closeModal(modals.UpdateDeleteModal)}>
+    onClick={() => closeModal(modals.ProfileUpdateModal)}>
       <div className='modal_under'>
       <button onClick={updateHandler}>앨범에서 선택</button>
         <hr className="w-[100%] border-black-30" />
         <button onClick={deleteHandler}>기본 이미지로 변경</button>
         <hr className="w-[100%] border-black-30" />
-        <button onClick={() => closeModal(modals.UpdateDeleteModal)}>
+        <button onClick={() => closeModal(modals.ProfileUpdateModal)}>
           취소
         </button>
       </div>
--- src/pages/mypage/MyPageUpdate.tsx
+++ src/pages/mypage/MyPageUpdate.tsx
@@ -1,10 +1,40 @@
-import MypageProfileImage from 'src/components/mypage/MypageProfileImage'
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import { getUserInfo } from 'src/apis/authApi';
+import { useModal } from 'src/hooks/useModal';
+import { modals } from 'src/components/modals/Modals';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import React, { useRef } from 'react'
+
+interface IProfile {
+  image : string
+  nickname : string
+}
 
 function MyPageUpdate() {
+  const userId = JSON.parse(localStorage.getItem('userId') || '{}');
+  const { data } = useQuery(['user'], () => getUserInfo(userId));
+  const { openModal } = useModal();
+
+  // form 생성
+  const { register, handleSubmit, setValue } = useForm<IProfile>({
+    mode: 'all',
+  });
+
+  // 사진 추가를 위한 Ref 생성
+  const fileInput = useRef(null)
   return (
     <div className="container">
-      <MypageProfileImage />
+      <form>
+        {/* 프로필 이미지 영역 */}
+        <input accept='image/*' className='hidden' id='image' type='file' ref={fileInput}/>
+          <img
+            src={data?.data.profileImg}
+            alt="profile"
+            className="h-[11.875rem] w-[11.875rem] rounded-full bg-[#F5F5F5] object-cover"
+            onClick={() => openModal(modals.ProfileUpdateModal)}
+            />
+
+      </form>
   </div>
   )
 }
