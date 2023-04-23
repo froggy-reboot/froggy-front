52de42ab3247fc6bf05d2874b8095bbf1c65b9c4
s0ojin <onepst@hanyang.ac.kr>
2023-04-23 20:16:49 +0900

    배포테스트 : 이젠진짜 되나??

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function Main() {
-  return (
-    <div>Main</div>
-  )
+  return <div>github action deploy test</div>;
 }
 
-export default Main
+export default Main;
