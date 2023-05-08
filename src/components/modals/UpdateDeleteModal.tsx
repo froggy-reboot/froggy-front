d133574dadd61d45374d31d44f5645365cf2883c
s0ojin <onepst@hanyang.ac.kr>
2023-05-08 14:03:11 +0900

    Design : 언더모달 모바일 safe sizing - bottom padding 수정

--- src/components/modals/UpdateDeleteModal.tsx
+++ src/components/modals/UpdateDeleteModal.tsx
@@ -53,11 +53,17 @@ export default function UpdateDeleteModal() {
       className="modal_bg items-end"
       onClick={() => closeModal(modals.UpdateDeleteModal)}>
       <div className="modal_under">
-        <button onClick={updateHandler}>수정하기</button>
+        <button className="modal_under_btn" onClick={updateHandler}>
+          수정하기
+        </button>
         <hr className="w-[100%] border-black-30" />
-        <button onClick={deleteHandler}>삭제하기</button>
+        <button className="modal_under_btn" onClick={deleteHandler}>
+          삭제하기
+        </button>
         <hr className="w-[100%] border-black-30" />
-        <button onClick={() => closeModal(modals.UpdateDeleteModal)}>
+        <button
+          className="modal_under_btn"
+          onClick={() => closeModal(modals.UpdateDeleteModal)}>
           취소
         </button>
       </div>
